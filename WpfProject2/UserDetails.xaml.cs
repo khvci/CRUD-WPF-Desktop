453296ea68416197f89d35e84f0ffc6d170821e6
khvci <88493840+khvci@users.noreply.github.com>
2023-02-12 19:18:59 +0300

    bug fix: add try-catch to new customer method

--- WpfProject2/UserDetails.xaml.cs
+++ WpfProject2/UserDetails.xaml.cs
@@ -22,30 +22,39 @@ namespace WpfProject2
         {
             using (SqlConnection connection = new SqlConnection(Connection.connectionString))
             {
-                connection.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax) " +
-                                                "VALUES (@id, @companyName, @contactName, @contactTitle, @address, @city, @region, @postalCode, @country, @phone, @fax)", connection);
-                cmd.Parameters.AddWithValue("@id", txtCustomerID.Text);
-                cmd.Parameters.AddWithValue("@companyName", txtCompanyName.Text);
-                cmd.Parameters.AddWithValue("@contactName", txtContactName.Text);
-                cmd.Parameters.AddWithValue("@contactTitle", txtContactTitle.Text);
-                cmd.Parameters.AddWithValue("@address", txtAddress.Text);
-                cmd.Parameters.AddWithValue("@city", txtCity.Text);
-                cmd.Parameters.AddWithValue("@region", txtRegion.Text);
-                cmd.Parameters.AddWithValue("@postalCode", txtPostalCode.Text);
-                cmd.Parameters.AddWithValue("@country", txtCountry.Text);
-                cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
-                cmd.Parameters.AddWithValue("@fax", txtFax.Text);
+                
 
-                int rowsAffected = cmd.ExecuteNonQuery();
-                if (rowsAffected > 0)
+                try
                 {
-                    MessageBox.Show("Customer added successfully.");
-                    this.Close();
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax) " +
+                                                    "VALUES (@id, @companyName, @contactName, @contactTitle, @address, @city, @region, @postalCode, @country, @phone, @fax)", connection);
+                    cmd.Parameters.AddWithValue("@id", txtCustomerID.Text);
+                    cmd.Parameters.AddWithValue("@companyName", txtCompanyName.Text);
+                    cmd.Parameters.AddWithValue("@contactName", txtContactName.Text);
+                    cmd.Parameters.AddWithValue("@contactTitle", txtContactTitle.Text);
+                    cmd.Parameters.AddWithValue("@address", txtAddress.Text);
+                    cmd.Parameters.AddWithValue("@city", txtCity.Text);
+                    cmd.Parameters.AddWithValue("@region", txtRegion.Text);
+                    cmd.Parameters.AddWithValue("@postalCode", txtPostalCode.Text);
+                    cmd.Parameters.AddWithValue("@country", txtCountry.Text);
+                    cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                    cmd.Parameters.AddWithValue("@fax", txtFax.Text);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Customer added successfully.");
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to add customer.");
+                    }
                 }
-                else
+                catch (System.Exception)
                 {
-                    MessageBox.Show("Failed to add customer.");
+                    MessageBox.Show("This ID has already been used. Please select another ID.");
                 }
             }
         }
